3fa17a0c24e3f7ec27f6cf2e5f22ee58cdcb68f7
Helen Wang <hewangintern@jumptrading.com>
2025-07-24 19:57:16 +0000

    add I/O test for bundle

--- src/disco/pack/test_pack_tile.c
+++ src/disco/pack/test_pack_tile.c
@@ -109,6 +109,12 @@ action_t * ACTIONS[] = {
 #include "fd_pack_cost.h"
 const char SIGNATURE_SUFFIX[ FD_TXN_SIGNATURE_SZ - sizeof(ulong) - sizeof(uint) ] = ": this is the fake signature of transaction number ";
 const char WORK_PROGRAM_ID[ FD_TXN_ACCT_ADDR_SZ ] = "Work Program Id Consumes 1<<j CU";
+const char COMMISSION_PUBKEY[ 32 ] = { 1,2,3,4,5,6,7,8,9,10,
+                                      11,12,13,14,15,16,17,18,
+                                      19,20,21,22,23,24,25,26,
+                                      27,28,29,30,31,32 };
+const char COMMISSION              = 2;
+static ulong signer                = 0;
 #define MAX_TEST_TXNS (26)
 static fd_txn_p_t txn_scratch[ MAX_TEST_TXNS ] = {0};
 static ulong      txnp_sz[ MAX_TEST_TXNS     ] = {0};
@@ -196,7 +202,7 @@ make_transaction1( fd_txn_p_t * txnp,
   t->acct_addr_off          = FD_TXN_SIGNATURE_SZ+1UL;
 
   /* Add the signer */
-  *p = 's' + 0x80; fd_memcpy( p+1, &i, sizeof(ulong) ); memset( p+9, 'S', FD_TXN_ACCT_ADDR_SZ-9 ); p += FD_TXN_ACCT_ADDR_SZ;
+  *p = 's'; fd_memcpy( p+1, &signer, sizeof(ulong) ); memset( p+9, 'S', FD_TXN_ACCT_ADDR_SZ-9 ); p += FD_TXN_ACCT_ADDR_SZ;
   /* Add the writable accounts */
   for( ulong i = 0UL; writes[i] != '\0'; i++ ) {
     memset( p, writes[i], FD_TXN_ACCT_ADDR_SZ );
@@ -231,7 +237,7 @@ make_transaction1( fd_txn_p_t * txnp,
 
   t->instr[ 1 ].program_id = prog_start;
   t->instr[ 1 ].acct_cnt   = 0;
-  t->instr[ 1 ].data_sz    = 9;     // "2" and then "rewards_per_cu"
+  t->instr[ 1 ].data_sz    = 9;     // "3" and then "rewards_per_cu"
   t->instr[ 1 ].acct_off   = (ushort)(p - p_base);
   t->instr[ 1 ].data_off   = (ushort)(p - p_base);
 
@@ -270,9 +276,30 @@ make_transaction( fd_txn_p_t * txnp,
   double priority     = MAX_PRIORITY - (double)i*0.2;
   FD_TEST( priority>0 );
   ulong  priority_fee = make_transaction1( txnp, i, 500U, 500U, priority, w_accs, r_accs );
-  FD_LOG_NOTICE(( "make_transaction. priority: %lf, reward: %lu", priority, priority_fee ));
+  FD_LOG_NOTICE(( "make_transaction_%lu, priority: %lf, reward: %lu", i, priority, priority_fee ));
+  ++signer;
   return priority_fee;
 }
+
+/* make a bundle with 'bundle_txn_cnt' number of txnes starting at
+   'bundle_txn_i_start' in the txn_scratch. */
+static void
+make_bundle( int bundle_txn_cnt,
+             int bundle_txn_i_start ) {
+
+  FD_TEST( (ulong)bundle_txn_cnt<=FD_PACK_MAX_TXN_PER_BUNDLE );
+  FD_TEST( 0<=bundle_txn_i_start && bundle_txn_i_start+bundle_txn_cnt <= MAX_TEST_TXNS );
+
+  fd_txn_p_t * txnp = txn_scratch + bundle_txn_i_start;
+
+  const char empty_w_acc[1] = {0};
+  for( int i=bundle_txn_i_start; i<bundle_txn_i_start+bundle_txn_cnt; i++ ) {
+    make_transaction( txnp, (ulong)i, empty_w_acc, accs[i] );
+    FD_TEST( txnp->payload_sz );
+    txnp++;
+  }
+}
+
 
 static void
 make_leader( fd_became_leader_t * leader ) {
@@ -297,20 +324,20 @@ print_insert( fd_pack_ctx_t * pack_ctx ) {
   ulong delete_cnt;
   ulong prev_reward = ULONG_MAX;
 
-  for( int txn_i=0; txn_i<MAX_TEST_TXNS; txn_i++ ) {
-    FD_LOG_NOTICE(( "R acc: %s", accs[ txn_i ] ));
+  for( int i=0; i<MAX_TEST_TXNS; i++ ) {
+    FD_LOG_NOTICE(( "R acc: %s", accs[ i ] ));
 
     fd_txn_e_t * cur_spot = fd_pack_insert_txn_init( pack_ctx->pack );
-    ulong curr_reward = make_transaction( &txn_scratch[txn_i], (ulong)txn_i, empty_w_acc, accs[ txn_i ] );
+    ulong curr_reward = make_transaction( &txn_scratch[i], (ulong)i, empty_w_acc, accs[ i ] );
     FD_TEST( curr_reward < prev_reward );
     prev_reward = curr_reward;
 
-    fd_txn_p_t * txnp     = &txn_scratch[txn_i];
+    fd_txn_p_t * txnp     = &txn_scratch[i];
     fd_txn_t   * txn      = TXN( txnp );
-    txnp_sz[txn_i]        = txnp->payload_sz;
-    txnt_sz[txn_i]        = (ushort) fd_txn_footprint( txn->instr_cnt, txn->addr_table_adtl_cnt );
-    fd_memcpy( cur_spot->txnp->payload, txnp, txnp_sz[txn_i] );
-    fd_memcpy( TXN(cur_spot->txnp),     txn,  txnt_sz[txn_i] );
+    txnp_sz[i]        = sizeof(fd_txn_p_t);
+    txnt_sz[i]        = (ushort) fd_txn_footprint( txn->instr_cnt, txn->addr_table_adtl_cnt );
+    fd_memcpy( cur_spot->txnp->payload, txnp, txnp_sz[i] );
+    fd_memcpy( TXN(cur_spot->txnp),     txn,  txnt_sz[i] );
     fd_pack_insert_txn_fini( pack_ctx->pack, cur_spot, 0, &delete_cnt );
   }
 
@@ -357,7 +384,7 @@ main( int     argc,
 
   for( ulong i=0UL; i<config->topo.wksp_cnt; i++ ) {
     fd_topo_wksp_t * wksp = &config->topo.workspaces[ i ];
-    FD_LOG_NOTICE(( "Creating workspace %s (--page-cnt %lu, --page-sz %lu, --cpu-idx %lu)", wksp->name, wksp->page_cnt, wksp->page_sz, fd_shmem_cpu_idx( wksp->numa_idx ) ));
+    // FD_LOG_NOTICE(( "Creating workspace %s (--page-cnt %lu, --page-sz %lu, --cpu-idx %lu)", wksp->name, wksp->page_cnt, wksp->page_sz, fd_shmem_cpu_idx( wksp->numa_idx ) ));
     wksp->wksp            = fd_wksp_new_anonymous( wksp->page_sz,  wksp->page_cnt, fd_shmem_cpu_idx( wksp->numa_idx ), wksp->name, 0UL );
     FD_TEST( wksp->wksp );
     ulong offset          = fd_wksp_alloc( wksp->wksp, fd_topo_workspace_align(), wksp->known_footprint, 1UL );
@@ -454,14 +481,173 @@ main( int     argc,
     .seqs         = stem_seq
   };
 
-  int txn_i                 = 0;  // points to the next free slot in txn_scratch
+  int txn_i                 = 0;
   int txn_ref_i             = 0;  // reference txn to check against
   int txn_in_pack           = 0;  // number of txnes currently stored in pack
   int txn_in_pack_at_leader = 2;  // least number of txnes stored in pack when becoming the leader.
 
   const char empty_w_acc[1] = {0};
 
-  /* [tile-unit-test] */
+  /* [tile-unit-test] Bundle */
+  FD_LOG_NOTICE(( "--------------------[tile-unit-test] Bundle----------------------" ));
+  for( int txn_per_bundle = FD_PACK_MAX_TXN_PER_BUNDLE; txn_per_bundle>=1; txn_per_bundle--  ) {
+    ulong bundle_id        = (ulong)txn_per_bundle;  // current bundle id
+    int bundle_txn_i_start = (int)txn_i;
+    int bundle_txn_i_end   = (int)txn_i + txn_per_bundle;
+    make_bundle( txn_per_bundle, bundle_txn_i_start );
+
+    /* publish the bundle */
+    while( (int)txn_i<bundle_txn_i_end ) {
+      fd_txn_p_t * txnp    = &txn_scratch[txn_i];
+      fd_txn_t   * txn     = TXN( txnp );
+      txnp_sz[txn_i]       = txnp->payload_sz;
+      FD_TEST( txnp_sz[txn_i] );
+      txnt_sz[txn_i]       = (ushort) fd_txn_footprint( txn->instr_cnt, txn->addr_table_adtl_cnt );
+      fd_txn_m_t * txnm    = (fd_txn_m_t *) fd_chunk_to_laddr( resolve_link_base, resolve_chunk );
+      txnm->payload_sz     = (ushort) txnp_sz[txn_i];
+      txnm->txn_t_sz       = (ushort) txnt_sz[txn_i];
+      fd_memcpy( fd_txn_m_payload( txnm ), txnp,  sizeof(fd_txn_p_t) );
+      fd_memcpy( fd_txn_m_txn_t(   txnm ), txn,   txnm->txn_t_sz     );
+      ulong txnm_footprint = fd_txn_m_realized_footprint( txnm, 1, 0 );
+      /* bundle block engine */
+      txnm->block_engine.bundle_id      = bundle_id;
+      txnm->block_engine.bundle_txn_cnt = (ulong)txn_per_bundle;
+      txnm->block_engine.commission     = COMMISSION;
+      fd_memcpy( txnm->block_engine.commission_pubkey, COMMISSION_PUBKEY, 32 );
+
+      fd_mcache_publish( resolve_mcache, resolve_depth, resolve_seq, 0, resolve_chunk, txnm_footprint, 0, 0, 0 );
+
+      ulong in_idx   = IN_KIND_RESOLV;
+      ulong in_sig   = 0;               // for testing. should be the blockhash from resolve tile
+      ulong in_sz    = txnm_footprint;
+      ulong in_chunk = resolve_chunk;
+
+      resolve_chunk = fd_dcache_compact_next( resolve_chunk, txnm_footprint, resolve_chunk0, resolve_wmark  );
+      resolve_seq   = fd_seq_inc( resolve_seq, 1 );
+      txn_i++;
+
+      // before/after_credit should be like a no-op right now since we don't test overrun and haven't became a leader yet
+      int charge_busy = 1;
+      FD_LOG_NOTICE(( "before_credit %d", txn_i-1 ));
+      before_credit( pack_ctx, &stem, &charge_busy );
+      FD_LOG_NOTICE(( "after_credit %d", txn_i-1 ));
+      after_credit( pack_ctx, &stem, 0, &charge_busy );
+
+      FD_LOG_NOTICE(( "during_frag %d", txn_i-1 ));
+      during_frag( pack_ctx, in_idx, 0, in_sig, in_chunk, in_sz, 0 );
+
+      /* Verify insertion from pack_ctx */
+      FD_TEST( pack_ctx->is_bundle                                                               );
+      FD_TEST( pack_ctx->current_bundle->id                ==bundle_id                           );
+      FD_TEST( pack_ctx->current_bundle->txn_cnt           ==(ulong)txn_per_bundle               );
+      FD_TEST( pack_ctx->current_bundle->txn_received      ==(ulong)(txn_i-bundle_txn_i_start-1) );
+      FD_TEST( pack_ctx->current_bundle->min_blockhash_slot==in_sig                              );
+
+      FD_TEST( pack_ctx->blk_engine_cfg->commission==COMMISSION                                             );
+      FD_TEST( fd_memeq( pack_ctx->blk_engine_cfg->commission_pubkey->b, COMMISSION_PUBKEY, 32 ) );
+
+      fd_txn_p_t * txnp_during_frag = &txn_scratch[ txn_i-1 ];
+      FD_TEST( pack_ctx->cur_spot->txnp->payload_sz==txnp_sz[ txn_i-1 ]                            );
+      FD_TEST( fd_memeq( pack_ctx->cur_spot->txnp->payload, txnp_during_frag,  txnp_sz[ txn_i-1 ] ) );
+      FD_TEST( fd_memeq( TXN(pack_ctx->cur_spot->txnp), TXN(txnp_during_frag), txnt_sz[ txn_i-1 ] ) );
+
+      FD_LOG_NOTICE(( "after_frag %d", txn_i-1 ));
+      after_frag( pack_ctx, in_idx, 0, in_sig, 0, 0, 0, &stem );
+
+      // Hack:
+      pack_ctx->skip_cnt         = 0;  // no skipping block
+      pack_ctx->bank_idle_bitset = 1;  // bank is always "idle"
+    }
+
+    fd_pack_set_initializer_bundles_ready( pack_ctx->pack );
+
+    // Mock PoH tile: become the leader
+    FD_LOG_NOTICE(( "Bundle Ieration %lu: IN_KIND_POH", bundle_id ));
+    fd_became_leader_t * leader = (fd_became_leader_t *) fd_chunk_to_laddr( poh_link_base, poh_chunk );
+    make_leader( leader );
+
+    ulong in_idx   = IN_KIND_POH;
+    ulong in_chunk = poh_chunk;
+    ulong in_sz    = sizeof(fd_became_leader_t);
+    ulong in_sig   = fd_disco_poh_sig( (ulong)bundle_id, POH_PKT_TYPE_BECAME_LEADER, 0UL );
+
+    poh_chunk = fd_dcache_compact_next( poh_chunk, sizeof(fd_became_leader_t), poh_chunk0, poh_wmark );
+    poh_seq   = fd_seq_inc( poh_seq, 1 );
+
+    // before/after_credit should be like a no-op right now since we don't test overrun and haven't became a leader yet
+    int charge_busy = 1;
+    FD_LOG_NOTICE(( "before_credit %lu", bundle_id ));
+    before_credit( pack_ctx, &stem, &charge_busy );   // should be like a no-op right now since we don't test overrun yet
+    FD_LOG_NOTICE(( "after_credit %lu", bundle_id ));
+    after_credit( pack_ctx, &stem, 0, &charge_busy );
+
+    FD_LOG_NOTICE(( "during_frag %lu", bundle_id ));
+    during_frag( pack_ctx, in_idx, 0, in_sig, in_chunk, in_sz, 0 );
+    /* Verify became_leader is copied over */
+    FD_TEST( fd_memeq( pack_ctx->_became_leader, leader, sizeof(fd_became_leader_t) ) );
+
+    FD_LOG_NOTICE(( "after_frag %lu", bundle_id ));
+    after_frag( pack_ctx, in_idx, 0, in_sig, 0, 0, 0, &stem );
+    FD_TEST( pack_ctx->leader_slot!=ULONG_MAX );
+
+    // Hack:
+    pack_ctx->skip_cnt         = 0;  // no skipping block
+    pack_ctx->bank_idle_bitset = 1;  // bank is always "idle"
+
+    after_credit( pack_ctx, &stem, 0, &charge_busy );
+
+    /* Test publishing to bank (logic taken by fd_bank_tile.c) */
+    fd_frag_meta_t * bank_mline        = bank_mcache + fd_mcache_line_idx( bank_seq, bank_depth );
+    ushort           mline_sz          = bank_mline->sz;
+    ulong            bank_dcache_entry = (ulong)fd_chunk_to_laddr( bank_link_base, bank_mline->chunk ) + bank_mline->ctl;
+    int              txnp_cnt_out      = (int)((mline_sz-sizeof(fd_microblock_bank_trailer_t))/sizeof(fd_txn_p_t));
+    fd_txn_p_t     * txnp_out          = (fd_txn_p_t *) bank_dcache_entry;
+    FD_TEST( txnp_out );
+    FD_TEST( txnp_cnt_out==txn_per_bundle );
+    for( int txnp_out_i=0; txnp_out_i<txn_per_bundle; txnp_out_i++ ) {
+      fd_txn_p_t     * txnp              = &txn_scratch[ txn_ref_i ];
+      ulong            payload_sz        = txnp_sz[      txn_ref_i ];
+      if( ( txnp_out->payload_sz!=payload_sz ) ||
+        !fd_memeq( txnp, txnp_out, txnp_out->payload_sz ) ) {
+      FD_LOG_HEXDUMP_NOTICE(( "txnp",     txnp,      payload_sz           ));
+      FD_LOG_HEXDUMP_ERR((    "txnp_out", txnp_out,  txnp_out->payload_sz ));
+      }
+      fd_txn_t * txn_out      = TXN( txnp_out );
+      fd_txn_t * txn          = TXN( txnp );
+      ulong txn_t_sz          = txnt_sz[ txn_ref_i ];
+      ulong txn_t_sz_out      = (ushort) fd_txn_footprint( txn_out->instr_cnt, txn_out->addr_table_adtl_cnt );
+      FD_TEST( txn_t_sz_out==txn_t_sz );
+      if( ( txn_t_sz_out!=txn_t_sz ) ||
+          !fd_memeq( txn, txn_out, txn_t_sz_out ) ) {
+        FD_LOG_HEXDUMP_NOTICE(( "txn",     &txn,     txn_t_sz     ));
+        FD_LOG_HEXDUMP_ERR((    "txn_out", &txn_out, txn_t_sz_out ));
+      }
+
+      txnp_out++;
+      txn_ref_i++;
+    }
+
+    // bank chunk incremented in after_credit
+    bank_seq      = fd_seq_inc( bank_seq,    1 );
+
+    FD_LOG_NOTICE(( "bank out memory verified" ));
+
+    /* manual housekeeping */
+    pack_ctx->approx_wallclock_ns = fd_log_wallclock();
+    // Hack:
+    pack_ctx->skip_cnt         = 0;  // no skipping block
+    pack_ctx->bank_idle_bitset = 1;  // bank is always "idle"
+    fd_memset( pack_ctx->wait_duration_ticks, 0, sizeof( pack_ctx->wait_duration_ticks ) );
+
+    pack_ctx->leader_slot = ULONG_MAX;
+  }
+
+
+  /* [tile-unit-test] Normal I/O*/
+  FD_LOG_NOTICE(( "--------------------[tile-unit-test] Normal I/O----------------------" ));
+  fd_memset( txn_scratch, 0, sizeof(txn_scratch) );
+  txn_i      = 0;
+  txn_ref_i  = 0;
   int loop_i = 0;
   while( txn_i < MAX_TEST_TXNS ) {
     fd_became_leader_t * leader = NULL;
